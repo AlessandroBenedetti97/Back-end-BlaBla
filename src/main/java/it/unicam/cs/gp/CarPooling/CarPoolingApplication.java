06952f1a514d5e751778fbed952b0f84cb1c2a3f
AlessandroSbarbati <126078147+AlessandroSbarbati@users.noreply.github.com>
2024-03-15 13:03:29 +0100

    Aggiunta JAVADOC

--- src/main/java/it/unicam/cs/gp/CarPooling/CarPoolingApplication.java
+++ src/main/java/it/unicam/cs/gp/CarPooling/CarPoolingApplication.java
@@ -3,8 +3,11 @@ package it.unicam.cs.gp.CarPooling;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Questa classe Main serve per far partire tutto il progetto
+ */
 @SpringBootApplication
-public class CarPoolingApplication {
+public class  CarPoolingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarPoolingApplication.class, args);
